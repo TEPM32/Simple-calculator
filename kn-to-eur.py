b4a72a211ec00ce22ccca201604ece5b2787c8e5
TEPM <tineugen@gmail.com>
2020-10-27 17:55:58 +0100

    € to kn converter added.

---
+++ kn-to-eur.py
@@ -0,0 +1,13 @@
+print("Easy currency converter: € to kn")
+
+while True:
+    Eu = int(input("Please enter the number of Euros: "))
+    kn = Eu * 7.5835
+    print("{0} Euros is/are {1} kunas.".format(Eu, kn))
+
+    choice = input("Would you like another conversion? (y or n): ")
+
+    if choice != "y":
+        break
+
+print("End of conversion. Thank your for using our converter.")
